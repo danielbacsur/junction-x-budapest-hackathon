bdffd636c275675cd2b99138caea2df3a0c02040
Daniel Bacsur <contact@danielbacsur.com>
2023-11-25 00:12:24 +0100

    feat: integrate file upload functionality using vercel blob

--- apps/deepfake/components/upload-form.tsx
+++ apps/deepfake/components/upload-form.tsx
@@ -1,13 +1,31 @@
 "use client";
 
+import { upload } from "@vercel/blob/client";
 import { useRef, type FormEvent } from "react";
 
 export function UploadForm() {
   const inputFileRef = useRef<HTMLInputElement>(null);
 
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Form submitted");
+
+    if (!inputFileRef.current?.files) {
+      console.error("No file selected");
+      return;
+    }
+
+    const file = inputFileRef.current.files[0];
+
+    try {
+      const { url } = await upload(file.name, file, {
+        access: "public",
+        handleUploadUrl: "/api/upload",
+      });
+
+      console.log("Uploaded file URL:", url);
+    } catch (error) {
+      console.error("File upload failed:", error);
+    }
   };
 
   return (
