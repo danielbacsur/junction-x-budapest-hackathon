5c5ba40a64e09fad4599cf0834d6698dd64be9dd
Daniel Bacsur <contact@danielbacsur.com>
2023-11-24 21:27:17 +0100

    feat: add utility function to format time ago from a given date

--- apps/deepfake/lib/utils/index.ts
+++ apps/deepfake/lib/utils/index.ts
@@ -18,3 +18,30 @@ export function valid(tags: Tag[]) {
   return tags.length === 0;
 }
 
+export function timeAgo(date: Date): string {
+  const now = new Date();
+  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  let interval = seconds / 31536000; // 365 * 24 * 60 * 60
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000; // 30 * 24 * 60 * 60
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400; // 24 * 60 * 60
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600; // 60 * 60
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
