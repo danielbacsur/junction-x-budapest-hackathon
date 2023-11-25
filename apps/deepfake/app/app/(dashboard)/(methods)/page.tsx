ba3e4e8cf7742d43e0741c56cf92633145e7af48
Daniel Bacsur <contact@danielbacsur.com>
2023-11-25 01:02:13 +0100

    feat: create page with upload form and record link

---
+++ apps/deepfake/app/app/(dashboard)/(methods)/page.tsx
@@ -0,0 +1,25 @@
+import { UploadForm } from "@/components/upload-form";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function WorkspacesPage() {
+  return (
+    <div className="h-full flex">
+      <div className="flex-1 grid place-items-center">
+        <div className="flex flex-col items-center space-y-4">
+          <h1>Intuitech&apos;s Method</h1>
+          <UploadForm />
+        </div>
+      </div>
+      <div className="flex-1 grid place-items-center">
+        <div className="flex flex-col items-center space-y-4">
+          <h1>Discover Our Method</h1>
+
+          <Link href="/record">
+            <Button>Try It Out</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
