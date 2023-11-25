3acfa40375b6346604597ae8fdeda1e429ede6d1
Daniel Bacsur <contact@danielbacsur.com>
2023-11-25 02:37:14 +0100

    feat: store validation and associated tags in the database

--- apps/deepfake/app/api/verify/route.ts
+++ apps/deepfake/app/api/verify/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { prisma } from "@/lib/clients/prisma";
+import { getSession } from "@/lib/utils/auth";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const { url } = (await req.json()) as {
@@ -18,6 +20,36 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
   }).then((res) => res.json())) as {
     tags: { timestamp: number; type: string }[];
   };
+
+  const session = await getSession();
+
+  if (session) {
+    const validation = await prisma.validation.create({
+      data: {
+        video: url,
+        user: {
+          connect: {
+            id: session.user.id,
+          },
+        },
+      },
+    });
+
+    for (const tag of tags) {
+      await prisma.tag.create({
+        data: {
+          ...tag,
+          validation: {
+            connect: {
+              id: validation.id,
+            },
+          },
+        },
+      });
+    }
+
+    return NextResponse.json({ validation });
+  }
 
   return NextResponse.json({ tags });
 }
